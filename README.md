296968216f8414775d66c69dcbdcad22fce0724d
yuankaigou <602122923@qq.com>
2017-04-24 09:42:54 +0800

    add read me description

---
+++ README.md
@@ -0,0 +1,56 @@
+## YKScrollView
+
+
+`YKScrollView` is an iOS `Autoscroll Banner`. 
+
+
+*Advantages* 
+
+	- Just create Three ImageView object to display the images
+	- when you see the middle imageView the 2 images preload
+
+### Requirements
+
+`YKScrollView ` works on iOS 6+ and requires ARC to build. It depends on the following frameworks, which should  be included with most Project you work on:
+
+* SDWebImage
+
+
+MayBe After some versions this dependency will be removed.
+
+
+### Usage
+
+```objc
+    YKCycleScrollView * cycleScrollView = [[YKCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 150)];
+    
+    [cycleScrollView setImageUrlNames:@[imagePath1, imagePath2, imagePath3, imagePath4] animationDuration:3];
+    
+    [self.view addSubview:cycleScrollView];
+```
+
+Everything have been done, you can also setting style like this
+
+
+
+``` objc
+	 //You can setting the color like PageControl
+    cycleScrollView.pageControl.normalColor = [UIColor greenColor];
+    cycleScrollView.pageControl.selectedColor = [UIColor redColor];
+    
+    //You can Change the pointStyle By your self
+    cycleScrollView.pageControl.pointStyle = YTPageControlPointStyleRectangle;
+```
+
+If you want to observe the imageView click, you can the delegate like `UITableView`
+
+The protocol is `YKCycleScrollViewDelegate`
+
+```
+- (void)cycleScrollView:(YKCycleScrollView *)cycleScrollView DidTapImageView:(NSInteger)index
+{
+	//the index is the image order you tap
+}
+```
+
+
--- YKScroll/YKScroll/ViewController.m
+++ YKScroll/YKScroll/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "YKCycleScrollView.h"
 
-@interface ViewController ()
+@interface ViewController ()<YKCycleScrollViewDelegate>
 
 @end
 
@@ -20,10 +20,11 @@
     // Do any additional setup after loading the view, typically from a nib.
     YKCycleScrollView * cycleScrollView = [[YKCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 150)];
     
-    [cycleScrollView setImageUrlNames:@[@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958399976&di=2a395dc3a4256837868687c000ca2bbf&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2012%2F0428%2F78%2F18.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958445576&di=15f9cfe2dd302eda44f9d38455c902f6&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa2cc7cd98d1001e9594a3ab7b80e7bec54e79717.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958483218&di=9a95721c5cddc0947020fb532d754e07&imgtype=0&src=http%3A%2F%2Fff.topit.me%2Ff%2F68%2Ff3%2F11179295808c0f368fo.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958483218&di=677965d930c66de4ce726180167dfe2d&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F1b4c510fd9f9d72a3762d754d22a2834349bbb71.jpg"] animationDuration:1];
+    [cycleScrollView setImageUrlNames:@[@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958399976&di=2a395dc3a4256837868687c000ca2bbf&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2012%2F0428%2F78%2F18.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958445576&di=15f9cfe2dd302eda44f9d38455c902f6&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fa2cc7cd98d1001e9594a3ab7b80e7bec54e79717.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958483218&di=9a95721c5cddc0947020fb532d754e07&imgtype=0&src=http%3A%2F%2Fff.topit.me%2Ff%2F68%2Ff3%2F11179295808c0f368fo.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492958483218&di=677965d930c66de4ce726180167dfe2d&imgtype=0&src=http%3A%2F%2Fh.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F1b4c510fd9f9d72a3762d754d22a2834349bbb71.jpg"] animationDuration:3];
     
     cycleScrollView.pageControl.normalColor = [UIColor greenColor];
     cycleScrollView.pageControl.selectedColor = [UIColor redColor];
+    
     cycleScrollView.pageControl.pointStyle = YTPageControlPointStyleRectangle;
     
     
@@ -32,6 +33,11 @@
     
 }
 
+
+- (void)cycleScrollView:(YKCycleScrollView *)cycleScrollView DidTapImageView:(NSInteger)index
+{
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
